59a6bc8265b587bdd8e9fd2c46570cf82d3a6e1f
ReeceSpencer <57555952+ReeceSpencer@users.noreply.github.com>
2019-11-13 21:12:16 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include "BST.H"
+#include "DATE.H"
+#include "VECTOR.H"
+#include "windlog.h"
+#include "choices.H"
+
+using namespace std;
+
+int main()
+{
+    Choices choice;
+    return choice.MakeChoice();
+}
